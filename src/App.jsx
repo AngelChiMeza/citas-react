f5a870888e1ea0cd0f0f48fdb28505cbfef3b918
AngelChiMeza <angel.chi@uimqroo.edu.mx>
2024-01-17 16:42:20 -0500

    avance

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react"
 import ListadoPacientes from "./components/ListadoPacientes"
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 
+
+
 function App() {
- 
+      const [pacientes, setPacientes] = useState([]);
+      const [paciente, setPaciente] = useState({});
 
   return (
     <div className="container mx-auto mt-20">
       
       <Header />
       <div className="mt-12 md:flex">
-      <Formulario />
-      <ListadoPacientes />
+      <Formulario 
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+      />
+      <ListadoPacientes 
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          //eliminarPaciente={eliminarPaciente}
+      />
       </div>
     </div>
   )
